00c6e556d49d6f442257d5bb0a24418a6cd74f13
Mehrac Lətifli <meracletifli@outlook.com>
2021-05-06 18:00:24 +0400

    Add project files.

---
+++ Singleton pattern/PersonalRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton_pattern
+{
+    sealed class PersonalRepository
+    {
+        private PersonalRepository() { }
+
+        private static PersonalRepository _instance;
+
+
+        public static PersonalRepository Getinstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new PersonalRepository();
+            }
+
+            return _instance;
+        }
+
+        List<Person> people = new List<Person>();
+
+        public void Add(Person person)
+        {
+
+            people.Add(person);
+
+        }
+
+        public List<Person> GetAll()
+        {
+            return people;
+        }
+    }
+}
---
+++ Singleton pattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Singleton_pattern
+{
+    class Program
+    {
+
+        static void Foo1()
+        {
+            var rep = PersonalRepository.Getinstance();
+            rep.Add(new Person { Name = "Name", Age = 18, Surname = "Surname" });
+            rep.Add(new Person { Name = "Name2", Age = 20, Surname = "Surname2" });
+
+        }
+        static void Foo2()
+        {
+            var rep = PersonalRepository.Getinstance();
+
+            foreach (var item in rep.GetAll())
+            {
+                Console.WriteLine($"{item.Name} {item.Age} {item.Surname}");
+            }
+        }
+        static void Main(string[] args)
+        {
+        
+            Foo1();
+            Foo2();
+        }
+    }
+}
